82cc25958c5869ab9fcca22448d42e0b16e2d812
j42huynh <john_huynh1@hotmail.com>
2026-01-04 16:34:01 -0500

    chore: split day3 into part1 and part2

--- 2025/day3/day3.go
+++ 2025/day3/day3.go
@@ -23,26 +23,42 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	result := 0
+	banks := []string{}
 	for scanner.Scan() {
 		line := scanner.Text()
-
-		joltage := getMaxJoltage(line)
-		fmt.Println(joltage)
-		result += joltage
+		banks = append(banks, line)
 	}
 
-	fmt.Println(result)
+	fmt.Print("Part1: ")
+	fmt.Println(part1(banks))
+	fmt.Print("Part2: ")
+	fmt.Println(part2(banks))
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func getMaxJoltage(bank string) int {
+func part1(banks []string) int {
+	result := 0
+	for _, bank := range banks {
+		result += getMaxJoltage(bank, 2)
+	}
+	return result
+}
+
+func part2(banks []string) int {
+	result := 0
+	for _, bank := range banks {
+		result += getMaxJoltage(bank, 12)
+	}
+	return result
+}
+
+func getMaxJoltage(bank string, numBatteriesOn int) int {
 	maxJoltage := 0
-	for i := range 12 {
-		partialBank := bank[:len(bank)-(11-i)]
+	for i := range numBatteriesOn {
+		partialBank := bank[:len(bank)-(numBatteriesOn-1-i)]
 
 		maxDigit := -1
 		maxIndex := -1
